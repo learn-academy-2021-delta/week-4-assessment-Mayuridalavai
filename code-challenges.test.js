9de238c3ab033940dd2d28df17b9b402cd1a2edb
Mayuridalavai <mayuri.gj@gmail.com>
2021-10-03 19:45:38 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,6 +15,18 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+//describe method that lists the name of the function
+//test method describes what function does
+//an expect method calls the function to test arguments
+
+describe ("shuffleArray", () => {
+    test("given an array, remove the first item and shuffle the remaining", () => {
+  expect(shuffleArray(colors1)).toContain("yellow", "blue", "pink", "green")
+  expect(shuffleArray(colors2)).toContain("saffron", "aquamarine", "periwinkle", "indigo", "ochre")
+    })
+  })
+
+
 var colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
@@ -24,20 +36,74 @@ var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saf
 
 // b) Create the function that makes the test pass.
 
+//declare a function named shuffleArray
+//.shift() is used to remove the value at the 0 index of the array
+//for loop is uded to iterate 
+//math.floor and math.random is used to get random elements in array
+
+const shuffleArray = (array)=>{
+    array === array.shift()
+   for (var i = array.length - 1; i > 0; i--) {
+       var rand = Math.floor(Math.random() * (i + 1));
+       [array[i], array[rand]] = [array[rand], array[i]]
+   }
+    return array
+
+}
+console.log(shuffleArray(colors1));
+console.log(shuffleArray(colors2));
+
+
 
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-var nums2 = [109, 5, 9, 67 8, 24]
-// Expected output: [5, 109]
+//describe method that lists the name of the function
+//test method describes what function does
+//an expect method calls the function to test arguments
+
+describe("minMax",() => {
+    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order',() =>{
+    expect(minMax(nums1)).toEqual([-8, 90])
+    expect(minMax(nums2)).toEqual([5, 109])
+    })
+})
+
+
+ var nums1 = [3, 56, 90, -8, 0, 23, 6]
+ // Expected output: [-8, 90]
+ var nums2 = [109, 5, 9, 67, 8, 24]
+ // Expected output: [5, 109]
 
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
+
+//declare a function named minMax
+//declare variables maxValues and minValues to array[0]
+//for loop for iterating the array length
+//if and else if to check the max and min values in array
+
+const minMax = (array) =>{
+    let maxValues=array[0]
+    let minValues=array[0]
+ 
+    for (let i = 0; i < array.length; i++) {
+      if(array[i] < minValues){
+        minValues = array[i];
+      }
+      else if(array[i] >maxValues){
+        maxValues = array[i];
+      }
+      }
+    return [minValues,maxValues]
+    
+  }
+
+  console.log(minMax(nums1));
+  console.log(minMax(nums2));
 
 
 
@@ -45,10 +111,37 @@ var nums2 = [109, 5, 9, 67 8, 24]
 
 // a) Create a test with an expect statement using the variables provided.
 
+//describe method that lists the name of the function
+//test method describes what function does
+//an expect method calls the function to test arguments
+
+describe("duplicateValue",() => {
+    it('function that takes in two arrays as arguments and returns one array with no duplicate',() => {
+        expect(duplicateValue(testArray1,testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+        })
+})
+
+
 var testArray1 = [3, 7, 10, 5, 4, 3, 3]
 var testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+ // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
 
-// b) Create the function that makes the test pass.
+ // b) Create the function that makes the test pass.
+
+ //declare a function named duplicateValue
+ //declare variable named testArray3 
+ //concat is used to combine the testArray1 and testArray2
+ //for loop to iterate 
+//.splice is used to change the contents of an array by romoving or replacing existing elements
+const duplicateValue = (testArray1, testArray2)=>{
+    var testArray3 = testArray1.concat(testArray2);
+for (var i = 0; i < testArray3.length; ++i) {
+    for (var j = i + 1; j < testArray3.length; ++j) {
+        if (testArray3[i] === testArray3[j])
+        testArray3.splice(j--, 1);
+    }
+}
+return testArray3
+}
--- code_challenges.rb
+++ code_challenges.rb
@@ -3,6 +3,12 @@
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
 
+# declare a method named odd_even
+# take  input number as num
+# conditionals if and elsif to check numbers even or odd
+# print the numbers using p command
+# end statement to stop the if condition
+
 num1 = 7
 # Expected output: '7 is odd'
 num2 = 42
@@ -10,9 +16,30 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def odd_even(num)
+    if num % 2 == 0
+    "#{num} is even"
+    elsif num % 2 != 0 
+    "#{num} is odd"
+    end 
+end
+p odd_even(7)
+p odd_even(42)
+p odd_even(221)
+
+
+
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+
+# declare a method named str_vowels
+# take  input string
+# use .delete "aeiouAEIOU" to delete the vowels in the string 
+# end to stop the method 
+# print the output of each srting using 'p'
+
 
 album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -21,13 +48,37 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def str_vowels string
+string = string.delete "aeiouAEIOU" 
+end
+p str_vowels 'Rubber Soul'
+p str_vowels 'Sgt Pepper'
+p str_vowels 'Abbey Road'
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forwards or backwards. Use the test variables provided.
 
+# declare a method named is_palindrome
+# take  input string
+# use conditionals if/elsif to check given string is palindrome or not
+# end to stop the if conditionals and method 
+# print the output of each srting using 'p'
+
 is_palindrome1 = 'Racecar'
-# Expected output: 'Racecar is a palindrome'
+# # Expected output: 'Racecar is a palindrome'
 is_palindrome2 = 'LEARN'
-# Expected output: 'LEARN is not a palindrome'
+# # Expected output: 'LEARN is not a palindrome'
 is_palindrome3 = 'Rotator'
-# Expected output: 'Rotator is a palindrome'
+# # Expected output: 'Rotator is a palindrome'
+
+def is_palindrome string
+    if string.downcase == string.downcase.reverse
+        "#{string} is a palindrome."
+    else
+        "#{string} is not a palindrome."
+    end
+end
+
+p is_palindrome 'Racecar'
+p is_palindrome 'LEARN'
+p is_palindrome 'Rotator'
